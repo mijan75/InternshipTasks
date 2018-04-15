279cc2edb0e49ce32b176cfd159c26cda71231d0
Mijanur Rahaman <mijanurman750@gmail.com>
2018-04-15 14:50:01 +0600

    updated

--- ThymeleafDemo/src/main/java/com/example/ThymeleafDemo/repository/StakeHolderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ThymeleafDemo.repository;
-
-import com.example.ThymeleafDemo.model.StakeHolder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StakeHolderRepository extends JpaRepository<StakeHolder, Integer>{
-}

17e5cd919cc38db322c2537f54851ace34044ea0
Mijanur Rahaman <mijanurman750@gmail.com>
2018-04-25 14:18:11 +0600

    Updated

--- BillPaymentAdminPanel/src/main/java/com/example/BillPaymentAdminPanel/repository/StakeHolderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BillPaymentAdminPanel.repository;
-
-import com.example.BillPaymentAdminPanel.model.StakeHolder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StakeHolderRepository extends JpaRepository<StakeHolder, Integer>{
-}
